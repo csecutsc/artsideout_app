e618dfe97e9148c18464a4f0b72ae971bee0c579
Ammar <ammart500@gmail.com>
2020-08-04 15:42:17 -0400

    updated art page ui

--- lib/components/home/Sidebar.dart
+++ lib/components/home/Sidebar.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:artsideout_app/theme.dart';
 
+const int HOMEPAGE_INDEX = 10;
+
 class Sidebar extends StatefulWidget {
   @override
   _SidebarState createState() => _SidebarState();
+
+  Sidebar(this.onTabTapped);
+
+  final Function onTabTapped;
 }
 
 class _SidebarState extends State<Sidebar> {
@@ -11,7 +17,7 @@ class _SidebarState extends State<Sidebar> {
   Widget build(BuildContext context) {
     return Container(
       height: MediaQuery.of(context).size.height,
-      width: 85,
+      width: 100,
       decoration: BoxDecoration(
         color: asoPrimary,
       ),
@@ -25,38 +31,69 @@ class _SidebarState extends State<Sidebar> {
           SizedBox(
             height: 40.0,
           ),
-          Icon(
-            Icons.map,
-            size: 40.0,
+          IconButton(
+            icon: Icon(Icons.home),
+            iconSize: 40.0,
+            onPressed: () {
+              changeScreen(HOMEPAGE_INDEX);
+            },
           ),
           SizedBox(
             height: 25,
           ),
-          Icon(
-            Icons.search,
-            size: 40.0,
+          IconButton(
+            icon: Icon(Icons.map),
+            iconSize: 40.0,
+            onPressed: () {
+              changeScreen(0);
+            },
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            iconSize: 40.0,
+            onPressed: () {
+              changeScreen(0);
+            },
           ),
           SizedBox(height: 145),
-          Icon(
-            Icons.palette,
-            size: 40.0,
+          IconButton(
+            icon: Icon(Icons.palette),
+            iconSize: 40.0,
+            onPressed: () {
+              changeScreen(3);
+            },
           ),
           SizedBox(
             height: 25,
           ),
-          Icon(
-            Icons.local_activity,
-            size: 40.0,
+          IconButton(
+            icon: Icon(Icons.local_activity),
+            iconSize: 40.0,
+            onPressed: () {
+              changeScreen(5);
+            },
           ),
           SizedBox(
             height: 25,
           ),
-          Icon(
-            Icons.bookmark,
-            size: 40.0,
-          )
+          IconButton(
+            icon: Icon(Icons.bookmark),
+            iconSize: 40.0,
+            onPressed: () {
+              changeScreen(0);
+            },
+          ),
         ],
       ),
     );
+  }
+
+  changeScreen(int index) {
+    setState(() {
+      widget.onTabTapped(index);
+    });
   }
 }
