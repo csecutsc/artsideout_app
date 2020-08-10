23760608bf299a95d49f413d91588e2aede02c21
STalukder20 <shammotalukder4@gmail.com>
2020-08-09 22:52:35 -0400

    Updated code for activity detail widget.

--- lib/components/activity/ActivityDetailWidget.dart
+++ lib/components/activity/ActivityDetailWidget.dart
@@ -22,7 +22,8 @@ class _ActivityDetailWidgetState extends State<ActivityDetailWidget> {
     if (startTimeGiven == "") {
       return "ALL DAY";
     } else {
-      return TimeOfDay.fromDateTime(DateTime.parse(startTimeGiven)).format(context);
+      return TimeOfDay.fromDateTime(DateTime.parse(startTimeGiven))
+          .format(context);
     }
   }
 
@@ -30,7 +31,8 @@ class _ActivityDetailWidgetState extends State<ActivityDetailWidget> {
     if (endTimeGiven == "") {
       return "";
     } else {
-      return TimeOfDay.fromDateTime(DateTime.parse(endTimeGiven)).format(context);
+      return TimeOfDay.fromDateTime(DateTime.parse(endTimeGiven))
+          .format(context);
     }
   }
 
@@ -47,7 +49,7 @@ class _ActivityDetailWidgetState extends State<ActivityDetailWidget> {
     }
     return zone;
   }
-  
+
   @override
   Widget build(BuildContext context) {
     //variable; if profile page or activity page
@@ -155,16 +157,29 @@ class _ActivityDetailWidgetState extends State<ActivityDetailWidget> {
                     fontSize: 16.0,
                   ))),
           Container(
-              padding: EdgeInsets.fromLTRB(10, 10, 10, 25),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  for (var profile in widget.data.profiles)
-                    RichText(
+            padding: EdgeInsets.fromLTRB(0, 10, 10, 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                for (var profile in widget.data.profiles)
+                  ListTile(
+                    leading: Text(
+                      "${profile.name}",
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 14.0,
+                      ),
+                    ),
+                    trailing: RichText(
                         text: TextSpan(children: <TextSpan>[
                       TextSpan(
-                          text: "${profile.name} \t\t\tClick for more",
-                          //style: defaultStyle,
+                          text: "Click for more",
+                          style: TextStyle(
+                            color: Colors.red,
+                            decoration: TextDecoration.underline,
+                            decorationColor: Colors.red,
+                            decorationStyle: TextDecorationStyle.solid,
+                          ),
                           recognizer: TapGestureRecognizer()
                             ..onTap = () {
                               Navigator.push(
@@ -177,76 +192,17 @@ class _ActivityDetailWidgetState extends State<ActivityDetailWidget> {
                               );
                             })
                     ])),
-                  /* Text(
-                  //   i.name + "\t\t\t" + i.type,
-                  //   textAlign: TextAlign.left,
-                  // ),
-                  for (var j in widget.data.profiles)
-                    IconButton(
-                      icon: Icon(
-                        Icons.web,
-                      ),
-                      onPressed: () async {
-                        var url = j.social["website"];
-                        if (await canLaunch(url)) {
-                          await launch(url);
-                        } else {
-                          throw 'Could not launch';
-                        }
-                      },
-                    ),
-                  for (var k in widget.data.profiles)
-                    IconButton(
-                      icon: Icon(
-                        Icons.web,
-                      ),
-                      onPressed: () async {
-                        var url = k.social["pinterest"];
-                        if (await canLaunch(url)) {
-                          await launch(url);
-                        } else {
-                          throw 'Could not launch';
-                        }
-                      },
-                    )
-                ],
-              )),
-          Container(
-              padding: EdgeInsets.only(bottom: 25),
-              child: Row(
-                children: [
-                  for (var i in widget.data.profiles)
-                    Text(
-                      i.social.toString(),
-                      textAlign: TextAlign.end,
-                    ),
-                ],
-              )), */
-                  Divider(
-                    color: Colors.black,
-                    thickness: 1.0,
-                    height: 0.0,
-                    indent: 15.0,
-                    endIndent: 15.0,
                   ),
-                ],
-              )),
-          Container(
-              padding: EdgeInsets.all(25),
-              child: Row(
-                children: [
-                  for (var i in widget.data.profiles) Text(i.social.toString())
-                ],
-              )),
-          Divider(
-            color: Colors.black,
-            thickness: 1.0,
-            height: 0.0,
-            indent: 15.0,
-            endIndent: 15.0,
-          ),
-        ],
-      ),
-    );
+                Divider(
+                  color: Colors.black,
+                  thickness: 1.0,
+                  height: 0.0,
+                  indent: 15.0,
+                  endIndent: 15.0,
+                ),
+              ],
+            ),
+          )
+        ]));
   }
 }
