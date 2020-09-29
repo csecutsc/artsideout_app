3d5542cfa84110f45a174600750564c16d36b141
Kevin Shen <kevin_shen@hotmail.ca>
2020-09-29 05:45:55 -0400

    Fixed issue with placeholder images not showing up

--- lib/constants/PlaceholderConstants.dart
+++ lib/constants/PlaceholderConstants.dart
@@ -1,6 +1,6 @@
 class PlaceholderConstants {
   static const String GENERIC_IMAGE =
-      "assets/placeholders/generic_placeholder.png";
+      "assets/assets/placeholders/generic_placeholder.png";
   static const String PROFILE_IMAGE =
-      "assets/placeholders/profile_placeholder.png";
+      "assets/assets/placeholders/profile_placeholder.png";
 }
